ef8ebf5cd633bd892e31bf7d1eae88eeaa206e60
sirnusi <38861084+sirnusi@users.noreply.github.com>
2020-09-17 08:13:30 +0100

    new commit

--- blogapp/models.py
+++ blogapp/models.py
@@ -42,6 +42,9 @@ class Comment(models.Model):
     post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
     created_on = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        ordering = ['-created_on']
+
     def __str__(self):
         return '%s - %s' % (self.post.title, self.name)
 
